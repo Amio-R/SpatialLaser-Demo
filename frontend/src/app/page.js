f858bee319b9200cef092af1ad30c89461ef67ca
am4rahma <am4rahma@uwaterloo.ca>
2023-08-21 22:35:34 -0400

    completed view for client use. Radius slider and map picker to locate lat/lng

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,18 +1,19 @@
 import Image from 'next/image';
 import dynamic from 'next/dynamic';
 import {useMemo} from 'react';
+import LoadSVG from '../../public/spinning-circles.svg';
 
 export default function Home() {
   const Map =  dynamic(
     () => import('@/components/map'),
     { 
-      loading: () => <p>A map is loading</p>,
+      loading: () => <Image className="block m-auto" src={LoadSVG} width={150} height={150} />,
       ssr: false
     }
   )
   
   return (
-    <main className="flex min-h-screen text-center flex-col bg-[#202125] p-5">
+    <main className="flex min-h-screen text-center flex-col bg-[#202125]">
       <h1 className="text-7xl font-bold tracking-tight text-[#6091AF] p-5">Spatial Laser Demo</h1>
       <Map />
     </main>
--- frontend/src/components/map.js
+++ frontend/src/components/map.js
@@ -1,15 +1,47 @@
 'use client';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import { useState } from 'react';
+import { MapContainer, TileLayer, useMapEvents, Circle, CircleMarker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import '@/styles/map.css';
 
 export default function Map() {
-    const CollinCountyLocation = [33.174719, -96.591638];
+    const CollinCountyLocation = [33.188217, -96.556555];
+    const [locate, setLocate] = useState(CollinCountyLocation);
+    const [radius, setRadius] = useState(0)
+
+    function LocateCursor () {
+        useMapEvents({
+            click: (e) => {
+                // console.log(locate, e.latlng)
+                setLocate([e.latlng['lat'], e.latlng['lng']])
+            }
+        })
+    }
+
     return (
-        <div className="relative flex justify-center">
-            <MapContainer center={CollinCountyLocation} zoom={11} style={{height: 800, width: "75%"}}> 
-                <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-            </ MapContainer>
+        <div className=" justify-center">
+            <div className=" flex justify-center mt-2">
+                <MapContainer center={CollinCountyLocation} zoom={11} style={{height: 750, width: "75%"}} >
+                    <LocateCursor />
+                    <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        maxZoom={15}
+                    />
+                    <CircleMarker center={locate} radius={5} color="red" fill="red" fillOpacity={0.6} />
+                    <Circle center={locate} radius={radius} />
+                </ MapContainer>
+            </div>
+            <div className=" flex justify-center mt-5">
+                <label htmlFor="range" className="block text-2xl font-bold text-white dark:text-white">Radius: {radius/1000} km</label>
+            </div>
+            <div className=" flex justify-center mt-5">
+                <input id="range" type="range" min={0} max={100000}
+                    value={radius}
+                    onInput={e => setRadius(e.target.value)}
+                    onChange={e => setRadius(e.target.value)}
+                    className="w-8/12 h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
+                </input>
+            </div>
         </div>
     )
 }
